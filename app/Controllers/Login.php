a64fb9269e77486496012b8390560de04ee9f56e
dimfaqot <dimyfaqot@gmail.com>
2023-11-20 00:33:23 +0700

    First

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -32,10 +32,9 @@ class Login extends BaseController
             'role' => $q['role']
         ];
 
-        dd($data);
-
+        dd(base_url('home'));
         session()->set($data);
-        sukses(base_url('home'), 'Login sukses.');
+        sukses(base_url() . 'home', 'Login sukses.');
     }
 
     public function logout()
