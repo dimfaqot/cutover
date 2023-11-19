80d2711ffe8c463e168ecd2a97bb93e2c4a407c5
dimfaqot <dimyfaqot@gmail.com>
2023-11-20 00:41:46 +0700

    First

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,15 +6,14 @@ class Home extends BaseController
 {
     function __construct()
     {
-        // helper('functions');
-        // if (!session('id')) {
-        //     gagal(base_url('login'), 'Please Login!.');
-        // }
-        // check_role();
+        helper('functions');
+        if (!session('id')) {
+            gagal(base_url('login'), 'Please Login!.');
+        }
+        check_role();
     }
     public function index(): string
     {
-        dd(url());
         return view('home', ['judul' => 'Home']);
     }
 }
